82b68a43f348b23e8be043623cc202af5ec517e5
Poppinrubo <poppinrubo@163.com>
2017-03-05 16:10:46 +0800

    jquery.dataTable v0.2
    完成功能,优化部分代码,点击事件终于解决了。单独使用可以了,但是出现一个头疼的问题,一个页面使用多次,会发生冲突 相互影响,加下标存数组对象来识别， 很遗憾 ，我的内存8G从30%到了90%，浏览器蹦了，无响应 啊哦 就这样放弃使用下标来识别了 操作DOM节点费内存~

--- jquery.dataTable.js
+++ jquery.dataTable.js
@@ -1,52 +1,38 @@
 /***DataTable的封装 by PoppinRubo****************开始了***************/
-/*选择行 对象*/
-var RowData = {};
+/*选择行对象 对于复选框 外部可访问*/
+var CheckData = [];
+/*url返回的数据用于取出行数据*/
+var GetJSONData = null;
+
 (function (d) {/*使用之前记得要引入JQ*/
     /*调试模式,默认关闭*/
     var DebugMsg = false;
-    /*记住当前表格,全局用于跨方法*/
+    /*记住当前表格,内部方法方法*/
     var T = null;
+    /*记录配置*/
+    var TableObject = null;
     /**
      * @return {boolean}
      */
-    d.fn.DataTable = function (Object) {/*使用table绑定 用ID 或class 建议使用ID*/
-        /*d是传进来的元素 d(this)就是这个table*/
+    d.fn.DataTable = function (Object) {/*使用table绑定 注意要用使用ID*/
+        /*先清空*/
+        CheckData = [];
+        GetJSONData = null;
+        /*清空 end*/
         var Table = d(this);
+        Table.html("");
+        /*d是传进来的元素 d(this)就是这个table*/
         T = Table;
         /*URl 方式创建DataTable 该链接返回的数据为json格式*/
-        var url = Object.url;
+
         if (bind(Object.debug)) {/*是否开启调试模式*/
             DebugMsg = Object.debug;
         }
+        var url = Object.url;
         if (!bind(url)) {/*检查是否绑定URL*/
             debug("未绑定url");
             return false;
         }
-        /*加载效果开始*/
-        // var loading = '<div id="spinner" style="  margin: 100px auto;width: 50px;height: 60px;text-align: center;font-size: 10px;">' +
-        //     '<div class="rect1"></div>' +
-        //     '<div class="rect2"></div>' +
-        //     '<div class="rect3"></div>' +
-        //     '<div class="rect4"></div>' +
-        //     '<div class="rect5"></div>' +
-        //     '</div>';
-        // $("#spinner").find("div").css({
-        //     "background-color":" #67CF22",
-        //     "height": "100%",
-        //     "width": "6px",
-        //     "display": "inline-block",
-        //     "-webkit-animation": "stretchdelay 1.2s infinite ease-in-out",
-        //     "animation": "stretchdelay 1.2s infinite ease-in-out"
-        // });
-        // Table.append(loading);
-        // var css = document.createElement('style');/*创建style标签*/
-        // css.type='text/style';
-        // css.text='.selector{attr:value;}';
-        // document.getElementsByTagName('head')[0].appendChild(css);/*在head插入style标签 用于插入css3代码*/
-        // document.getElementsByTagName("style")[0].innerHTML+="@-webkit-keyframes stretchdelay {0%, 40%, 100% { -webkit-transform: scaleY(0.4) }20% { -webkit-transform: scaleY(1.0) }}"+
-        // "@keyframes stretchdelay {0%, 40%, 100% {transform: scaleY(0.4);-webkit-transform: scaleY(0.4);}  20% { transform: scaleY(1.0);-webkit-transform: scaleY(1.0);}}";
-
-        /*加载效果结束*/
         var columns = Object.columns;
         if (!bind(columns)) {/*检查是否绑定表格配置*/
             debug("未绑定columns");
@@ -54,7 +40,7 @@ var RowData = {};
         }
         /*生成表头*/
         /*插入表头行*/
-        Table.append("<tr id='dt_head'></tr>");
+        Table.append("<tr class='dt_head'></tr>");
         for (var h = 0; h < columns.length; h++) {
             /*列宽控制*/
             var w = Object.columns[h]["width"];
@@ -68,12 +54,31 @@ var RowData = {};
         }
         var check = Object.check;
         if (bind(check)) {/*检查是否开启复选框 生成表头*/
-            if (check[0]) {
-                Table.find("tr").eq(0).prepend("<td title='全选' style='width:25px'><input id='dt_check_all' type='checkbox'></td>");
+            if (check) {
+                Table.find("tr").eq(0).prepend("<td title='全选' style='width:25px'><input class='dt_check_all' type='checkbox'></td>");
             }
         }
+        /*加载效果开始*/
+        var loading = '<div id="dt_loading" style="position: absolute;left: 0;top:0;color:#232323;background:rgba(250, 250, 250, 0.7);z-index: 500;width: 100%;height: 100%;' +
+            'text-align: center;font-size: 14px;font-family:Helvetica Neue,Helvetica,Arial,PingFang SC,Hiragino Sans GB,WenQuanYi Micro Hei,Microsoft Yahei,sans-serif;">' +
+            '<span style="margin-top: 22%;position: absolute;"><span class="dt_animation" style="position:relative;width:20px;height:20px;background:#232323;">3</span></span>' +
+            '<span style="margin-top: 25%;position: absolute;">加载中...</span></div>';
+        Table.append(loading);
+        var dt_animation = $(".dt_animation");
+        var startAnimation = function () {
+            dt_animation.animate({left: 10}, "slow");
+            dt_animation.animate({left: 30}, "slow");
+            dt_animation.animate({left: 10}, "slow");
+            dt_animation.animate({left: 0}, "slow", startAnimation);
+        };
+        /*启动动画 卖个萌*/
+        startAnimation();
+        /*加载效果结束*/
         $.getJSON(url, function (json) {/*根据URL拉取数据*/
+            $("#dt_loading").remove();//移除动画
+            startAnimation = null;
             if (json.length) {
+                GetJSONData = json;
                 for (var r = 0; r < json.length; r++) {/*遍历行*/
                     /*table组装行*/
                     Table.append("<tr></tr>");
@@ -87,29 +92,21 @@ var RowData = {};
                             ColumnTitle = ColumnContent;
                         }
                         if (bind(facility)) {
-                            var DataBind = Object.columns[c]["bind"];
-                            /*检查是否设置绑定的字段*/
-                            if (!bind(DataBind)) {
-                                debug("未设置绑定字段");
-                                return false;
-                            }
-                            var DataID = json[r][DataBind];
                             if (facility == "edit") {
-                                ColumnContent = "<span data-id='" + DataID + "' class='dt_edit'>编辑</span>";
+                                ColumnContent = "<span  class='dt_edit'>编辑</span>";
                                 ColumnTitle = "编辑";
                             }
                             if (facility == "del") {
-                                ColumnContent = "<span data-id='" + DataID + "' class='dt_del'>删除</span>";
+                                ColumnContent = "<span class='dt_del'>删除</span>";
                                 ColumnTitle = "删除";
                             }
 
                         }
-                        /*生成一行一列 data-obj 为自定义标签用于储存对象 key*/
-                        var obj_key = [columns[c].ColumnName];
-                        Table.find("tr").eq(r + 1).append("<td data-obj='" + obj_key + "' title='" + ColumnTitle + "'>" + ColumnContent + "</td>");
+                        /*生成一行一列 data-row= 为自定义标签用于识别行 */
+                        Table.find("tr").eq(r + 1).append("<td data-row='" + parseInt(r + 2) + "'  title='" + ColumnTitle + "'>" + ColumnContent + "</td>");
                     }
                     if (bind(check)) {/*检查是否开启复选框 绑定行*/
-                        if (check[0]) {
+                        if (check) {
                             Table.find("tr").eq(r + 1).prepend("<td><input class='dt_checkbox' type='checkbox'></td>");
                         }
                     }
@@ -119,7 +116,10 @@ var RowData = {};
                     "border-collapse": "collapse",
                     "font-family": '"Microsoft YaHei", "微软雅黑", helvetica, arial, verdana, tahoma, sans-serif',
                     "font-size": "14px",
-                    "table-layout": "fixed"
+                    "table-layout": "fixed",
+                    "position": "absolute",
+                    "left": " 0",
+                    "top": "0"
                 });
                 Table.find("td").css({
                     "border": "1px solid #d8d8d8",
@@ -129,7 +129,13 @@ var RowData = {};
                     "cursor": "pointer",
                     "text-align": "left",
                 });
-                Table.find(".dt_checkbox,#dt_check_all").css({
+                Table.find("tr").eq(0).find("td").css({
+                    "cursor": "auto",
+                });
+                Table.find("tr").eq(0).find("td").eq(0).css({
+                    "cursor": "pointer",
+                });
+                Table.find(".dt_checkbox,.dt_check_all").css({
                     "width": "15px",
                     "height": "15px",
                     "margin": "0 auto",
@@ -148,14 +154,14 @@ var RowData = {};
                     "-ms-user-select": "none", /*IE10*/
                     "user-select": "none",
                     "color": "#ffffff",
-                    "z-index":"100"
+                    "z-index": "100"
                 });
                 var ButtonColor = Object.ButtonColor;
                 if (bind(ButtonColor)) {
                     Table.find("td span").css({"background": ButtonColor});
                 }
                 Table.find("tr").hover(function () {
-                    if (this.id != "dt_head") {
+                    if (this.className != "dt_head") {
                         this.style.backgroundColor = "#d8d8d8";
                     }
                 }, function () {
@@ -169,52 +175,90 @@ var RowData = {};
                 if (bind(align)) {
                     Table.find("td").css("text-align", align);
                 }
-                /*复选框控制 开始*/
-                /*行选*/
-                Table.find("td").click(function (ev) {
-                    if (ev.target === this&&this.parentNode.id!="dt_head") {/*防止事件错乱，并且不启用表头行选*/
-                        this.parentNode.firstChild.firstChild.click();
-                    }
-                });
-                /*点选*/
-                var dt_checkbox = $(".dt_checkbox");
-                dt_checkbox.click(function (ev) {
-                    /*获取该行所有列*/
-                    var TdNode = this.parentNode.parentNode.children;
-
-                    /*遍历提取对象*/
-                    for (var t = 0; t < TdNode.length; t++) {
-                        var key = TdNode[t].getAttribute("data-obj");
-                        if (key != null && key != "") {
-                            RowData[key] = TdNode[t].innerText;
+                if (bind(check)) {/*检查是否开启复选框 完成对应代码*/
+                    /*复选框控制 开始*/
+                    /*行选*/
+                    Table.find("td").click(function (ev) {
+                        if (ev.target === this && this.parentNode.className != "dt_head") {/*防止事件错乱，并且不启用表头行选*/
+                            this.parentNode.firstChild.firstChild.click();
                         }
-                    }
-
-
-                });
-                /*全选*/
-                var all = 0;
-                $("#dt_check_all").click(function () {
-                    if (all == 0) {
-                        dt_checkbox.click();
-                        dt_checkbox.prop("checked", true);
-                        all = 1;
-                    } else {
-                        dt_checkbox.click();
-                        dt_checkbox.prop("checked", false);
-                        all = 0;
-                    }
-                });
-                /*复选框控制 结束*/
-                function editClick() {
-                    $(".dt_edit").click(function () {/*编辑点击方法*/
-                        var data=TableGetVal(this);/*获取绑定 参数数据 点击行*/
-                        alert(data);
+                        if (ev.target === this && this.firstChild.className == "dt_check_all") {/*选所在td*/
+                            $(".dt_check_all").click();
+                        }
                     });
+                    /*点选*/
+                    var dt_checkbox = $(".dt_checkbox");
+                    dt_checkbox.click(function (ev) {
+                        /*点选加入到数组*/
+                        var data = GetClickRowData(this, 1);
+                        if (data != null) {
+                            CheckData.push(data);
+                        }
+
+                    });
+                    /*全选*/
+                    var all = 0;
+                    $(".dt_check_all").click(function () {
+                        if (all == 0) {
+                            dt_checkbox.click();
+                            dt_checkbox.prop("checked", true);
+                            all = 1;
+                        } else {
+                            dt_checkbox.click();
+                            dt_checkbox.prop("checked", false);
+                            all = 0;
+                        }
+                    });
+                    /*复选框控制 结束*/
                 }
-                $(".dt_del").click(function () {/*删除点击方法*/
-                    var data=TableGetVal(this);/*获取绑定 参数数据 点击行*/
+
+                /*******事件方法 开始*******/
+                $(".dt_edit").click(function () {/*编辑点击方法*/
+                    var data = GetClickRowData(this, 0);
+                    /*获取绑定 参数数据 点击行*/
+                    Object.editClick(data);
+
                 });
+                $(".dt_del").click(function () {/*删除点击方法*/
+                    var data = GetClickRowData(this, 0);
+                    /*获取绑定 参数数据 点击行*/
+                    Object.delClick(data);
+                });
+                /*定义setTimeout执行方法 解决单击双击冲突*/
+                var TimeFn = null;
+                Table.find("td").click(function (ev) {/*单击事件方法*/
+                    if (!bind(Object.Click)) {
+                        /*未开启单击*/
+                        return;
+                    }
+                    /*把它先储存 避免与setTimeout错乱*/
+                    var t = this;
+                    clearTimeout(TimeFn);
+                    /*执行延时*/
+                    TimeFn = setTimeout(function () {
+                        if (ev.target === t && t.lastChild.className != "dt_checkbox") {
+                            var data = GetClickRowData(t, 2);
+                            /*获取绑定 参数数据 点击行*/
+                            Object.Click(data);
+                        }
+                    }, 300);
+
+                });
+                Table.find("td").dblclick(function (ev) {/*双击事件方法*/
+                    if (!bind(Object.doubleClick)) {
+                        /*未开启双击*/
+                        return;
+                    }
+                    /*取消上次延时未执行的方法*/
+                    clearTimeout(TimeFn);
+                    /*双击事件的执行代码*/
+                    if (ev.target === this && this.lastChild.className != "dt_checkbox") {
+                        var data = GetClickRowData(this, 2);
+                        /*获取绑定 参数数据 点击行*/
+                        Object.doubleClick(data);
+                    }
+                });
+                /*******事件方法 结束*******/
             } else {
                 debug("返回数据为空");
             }
@@ -224,6 +268,59 @@ var RowData = {};
     /**下面是辅助方法**/
     function bind(b) {/*检查是否有绑定功能*/
         return typeof(b) != "undefined";
+    }
+
+    /*行点击操作*/
+    /*c 代表的是点谁 c用来找td t代表是区别操作类型 返回行对象*/
+    /*0 是编辑删除点击处理 1是复选框 2是单击取数据*/
+    /**
+     * @return {null|[]}
+     */
+    function GetClickRowData(c, t) {
+        var RowData = [];
+        var row = 0;
+        /*一行的所有列*/
+        if (t == 0) {
+            row = GerRowData(c.parentNode);
+        }
+        if (t == 1) {
+            row = GerRowData(c.parentNode.nextSibling);
+            /*检查是选择还是取消*/
+            if (!c.checked) {
+                /*如果取消 之前该行存在的元素要移除*/
+                var i = CheckData.length;
+                while (i--) {
+                    if (CheckData[i].LineNumber === row) {
+                        CheckData.splice(i, 1);
+                    }
+                }
+                return null;
+            }
+
+        }
+        if (t == 2) {
+            row = GerRowData(c);
+        }
+
+        if (row == 0) {
+            return null;
+        }
+        /*提取数据 组装对象*/
+        RowData = GetJSONData[row - 2];
+        RowData.LineNumber = row;
+        return RowData;
+    }
+
+    /*获取行号*/
+    /**
+     * @return {int}
+     */
+    function GerRowData(TdNode) {
+        var row = TdNode.getAttribute("data-row");
+        if (row != null && row != "") {
+            return row
+        }
+        return 0;
     }
 
     function debug(msg) {/*调试模式 错误提醒*/
@@ -259,15 +356,12 @@ var RowData = {};
 
     /**上面是辅助方法**/
 })(jQuery);
-/*外部调用方法*/
-/**
- * @return {string}
- */
-function TableGetVal(t) {
-
-    return t.getAttribute("data-id");
-}
-function GetRowData() {
-    return RowData;
-}
+/*外部访问方法*/
+/*返回的数组*/
+var GetJSONArray = function () {
+    return GetJSONData;
+};
+var GetCheckArray = function () {
+    return CheckData;
+};
 /***DataTable的封装 by PoppinRubo****************结束了***************/
